1717889a512f93620bc198503d09f6b4b9d7dcb8
SignatureJed <SignatureJed>
2021-02-19 18:36:03 +0700

    code complete

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -20,20 +20,82 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
+void importDataFromFile(string filename,vector<string> &nickname,vector<int> &score, vector<char> &mygrade)
+{    
+    ifstream source;
+    source.open(filename); 
+    string havetext;
+    while (getline(source,havetext))
+    {
+    const char * cStr  = havetext.c_str();
+    char format[] = "%[^:]: %d %d %d";
+    char name[100];
+    char grade;
+    int a,b,c;
+    sscanf(cStr,format,name,&a,&b,&c); 
+    int sumscore = a+b+c;
+    grade = score2grade(sumscore);
+    nickname.push_back(name);
+    score.push_back(sumscore);
+    mygrade.push_back(grade);
+    }
+
+
+    
+
+
+    
+}
+
+void getCommand(string &command,string &key)
+{   
+    string s;
+    char x[100];
+    char xx[100];
+    char format[] = "%s %[^\n]";
+    cout << "Please input your command: ";
+    
+    getline(cin,s);
+    sscanf(s.c_str(),format,x,xx);
+    command = x;
+    key = xx;
+    
+
 
 }
 
-void getCommand(){
+void searchName(vector<string> name,vector<int>score,vector<char>grade, string key)
+{   
+    bool stayhere =false;
+    for(unsigned int i =0; i < name.size(); i++)
+    {
+        if(key == toUpperStr(name[i]))
+        {   
+            cout << "---------------------------------" << endl;
+            cout << name[i] << "'s" << " score = " << score[i] << endl;
+            cout << name[i] << "'s" << " grade = " << grade[i] << endl;
+            cout << "---------------------------------" << endl;
+            stayhere = true;
+            break;
+        }   
+        
 
+    }  
+    if(stayhere == false) cout << "---------------------------------" << endl << "Cannot found." <<endl << "---------------------------------" << endl;
 }
 
-void searchName(){
-
-}
-
-void searchGrade(){
-
+void searchGrade(vector<string> name,vector<int>score,vector<char>grade, string key)
+{   
+    bool stayhere = false;
+    const char * cStr  = key.c_str();
+    cout << "---------------------------------" << endl;
+    for(unsigned int i =0; i < name.size(); i++) 
+    {
+        if(*cStr == grade[i]) cout << name[i] <<" (" << score[i] << ")" << endl;
+        stayhere = true;
+    }
+    if(stayhere == false) cout << "---------------------------------" << endl << "Cannot found." <<endl;
+    cout << "---------------------------------" << endl;
 }
 
 
